f98ea9cd031b0b271936913e5290ecc65f2cd0e3
hicelery <mungoharrison.ward9@gmail.com>
2026-04-07 18:30:07 +0100

    use default db

--- weather_proj/settings.py
+++ weather_proj/settings.py
@@ -79,7 +79,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'weather_proj.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
